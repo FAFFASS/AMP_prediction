4791aa511b821bf64c08bb3ca7df4b0980f34368
fanshen <49888775+FAFFASS@users.noreply.github.com>
2024-06-27 16:40:25 +0800

    Update Prediction-CNN_BiLSTM_Attention.py

--- prediction_script/Prediction-CNN_BiLSTM_Attention.py
+++ prediction_script/Prediction-CNN_BiLSTM_Attention.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# usage python Prediction_attention.py XX.vec XX.pred_out
+# usage python Prediction-CNN_BiLSTM_Attention.py XX.vec XX.pred_out
 
 from keras.models import load_model
 from numpy import loadtxt, savetxt
 from Attention import AttentionLayer
 from sys import argv
 
-input_data_path = 'XX.vec--'
-output_data_path = 'XX.pred_out'
+input_data_path = argv[1]
+output_data_path = argv[2]
 model = '../models/CNN_BiLSTM_Attention_model.h5'
 
 model = load_model(model, custom_objects={'AttentionLayer': AttentionLayer})
